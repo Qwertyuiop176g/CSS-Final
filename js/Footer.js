1e6cc268dfec33bb6503a01b50c95e014600d5a3
Qwertyuiop176g <ongkenny749@gmail.com>
2026-01-16 00:01:22 +0800

    Made some changes to the website

---
+++ js/Footer.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navHTML = `
+        <footer>
+            <div>
+                <p>Author: Group ?</p>
+                <p>Email: <a href="mailto:here@example.com">here@example.com</a></p>
+                <p>Credits:</p>
+                <a href="https://www.flaticon.com/free-icons/home" title="home icons">Home icons created by Dave Gandy - Flaticon</a>
+                <br>
+                <a href="https://www.freepik.com/icon/cursor_587335#fromView=family&page=1&position=3&uuid=7cf57952-11bf-492f-a90a-7c81bdc7b47e">Icon by Freepik</a>
+            </div>
+        </footer>
+    `;
+    
+    
+    const footerContainer = document.getElementById('footer-container');
+    if (footerContainer) {
+        footerContainer.innerHTML = navHTML;
+    }
+});
