8ed4fddf3d98e0c5151c9c269fea228bfbdc2952
enochenochngiam-a11y <enochenochngiam@gmail.com>
2026-01-20 13:53:58 +0800

    Add Planner link to navigation bar

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navHTML = `
         <nav>
+            <a href="Home.html" class="logo">
+                <img src="images/logo.png" alt="Singapore Attractions Logo">
+            </a>
             <a href="Home.html">Home</a>
             <a href="Attractions.html">Attractions</a>
             <a href="TourGuide.html">Tour Guides</a>
             <a href="AboutUs.html">About Us</a>
+            <a href="Planner.html">Planner</a>
         </nav>
     `;
     
