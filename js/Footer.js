b3f71a2d7cc21765fef2cdc690f65ab4ba756c30
Qwertyuiop176g <ongkenny749@gmail.com>
2026-01-20 12:56:52 +0800

    Made some changes to the website

--- js/Footer.js
+++ js/Footer.js
@@ -5,9 +5,11 @@ document.addEventListener('DOMContentLoaded', function() {
                 <p>Author: Group ?</p>
                 <p>Email: <a href="mailto:here@example.com">here@example.com</a></p>
                 <p>Credits:</p>
+                <div id="credits">
                 <a href="https://www.flaticon.com/free-icons/home" title="home icons">Home icons created by Dave Gandy - Flaticon</a>
                 <br>
                 <a href="https://www.freepik.com/icon/cursor_587335#fromView=family&page=1&position=3&uuid=7cf57952-11bf-492f-a90a-7c81bdc7b47e">Icon by Freepik</a>
+                </div>
             </div>
         </footer>
     `;
--- js/nav.js
+++ js/nav.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const navHTML = `
         <nav>
             <a href="Home.html">Home</a>
-            <a href="Attraction.html">Attractions</a>
+            <a href="Attractions.html">Attractions</a>
             <a href="TourGuide.html">Tour Guides</a>
             <a href="AboutUs.html">About Us</a>
         </nav>
